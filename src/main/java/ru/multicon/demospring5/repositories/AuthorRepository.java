5789d04ed4f027e6f7595d4ee22b34873237e584
Max Samkov <littlebiggame2@gmail.com>
2018-04-25 17:57:02 +0300

    Create two interfaces CrudRepository for book and author

--- src/main/java/ru/multicon/demospring5/repositories/AuthorRepository.java
+++ src/main/java/ru/multicon/demospring5/repositories/AuthorRepository.java
@@ -3,8 +3,6 @@ package ru.multicon.demospring5.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.multicon.demospring5.model.Author;
 
-import java.util.Optional;
-
 public interface AuthorRepository extends CrudRepository<Author, Long> {
 
 }
--- src/main/java/ru/multicon/demospring5/repositories/BookRepository.java
+++ src/main/java/ru/multicon/demospring5/repositories/BookRepository.java
@@ -3,5 +3,5 @@ package ru.multicon.demospring5.repositories;
 import org.springframework.data.repository.CrudRepository;
 import ru.multicon.demospring5.model.Book;
 
-public class BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
