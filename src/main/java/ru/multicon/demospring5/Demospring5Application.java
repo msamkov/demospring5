ed39051247220334d000b60703d5bcb016a5f9a1
Max Samkov <littlebiggame2@gmail.com>
2018-04-25 11:41:46 +0300

    Create a model of the Book and the Author

---
+++ src/main/java/ru/multicon/demospring5/Demospring5Application.java
@@ -0,0 +1,12 @@
+package ru.multicon.demospring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demospring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demospring5Application.class, args);
+	}
+}
